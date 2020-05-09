6fd4c6ebeb7d3875d9e612e13f925208df17673b
Luis Vazquez <lv120189@gmail.com>
2020-05-09 12:01:50 -0300

    objetivo cumplido

--- checkpoint.js
+++ checkpoint.js
@@ -1,113 +1,171 @@
-
 // Funciones
 
 function menorMayor(numeros) {
-  // Escribi una función llamada menorMayor que tome como entrada un arreglo de números y devuelva un arreglo 
-  // que contenga el menor número del arreglo en la posición cero y el mayor número del arreglo en la posición 1.
-  // Ej: menorMayor([4, 6, 1, 7, 15]) // retorna [1, 15]
+    // Escribi una función llamada menorMayor que tome como entrada un arreglo de números y devuelva un arreglo 
+    // que contenga el menor número del arreglo en la posición cero y el mayor número del arreglo en la posición 1.
+    // Ej: menorMayor([4, 6, 1, 7, 15]) // retorna [1, 15]
+    menor = numeros[numeros.length - 1];
+    mayor = 0;
+    newArray = [];
+    for (var i = 0; i < numeros.length; i++) {
+        if (mayor < numeros[i]) {
+            mayor = numeros[i];
+
+        }
+    }
+    for (var j = numeros.length; j >= 0; j--) {
+        if (menor > numeros[j]) {
+            menor = numeros[j];
+        }
+    }
+    newArray[0] = menor;
+    newArray[newArray.length] = mayor;
+    return newArray;
+
 
 }
 
 function stringMasLarga(strings) {
-  // Escribe una función llamada stringMasLarga, que tome un arreglo de strings llamado 'strings'
-  // tu función debe retornar el string más largo que hay en el arreglo
-  // Ej:
-  // stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); // returns 'guten tag'
-  // stringMasLarga(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
+    // Escribe una función llamada stringMasLarga, que tome un arreglo de strings llamado 'strings'
+    // tu función debe retornar el string más largo que hay en el arreglo
+    // Ej:
+    // stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); // returns 'guten tag'
+    // stringMasLarga(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
+    var masLarga = strings[0];
+    for (var i = 1; i < strings.length; i++) {
+        if (masLarga.length < strings[i].length) {
+            masLarga = strings[i];
+        }
+    }
+    return masLarga;
 
 }
 
 function buscarAmigo(amigos, nombre) {
-  // Escribe una funcion que reciba un array de amigos (objetos) y un nombre (string).
-  // Tu función debe devolver el amigo que coincida con el nombre recibido por argumento.
-  // Cada amigo tiene las propiedades nombre y edad.
-  // Ej:
-  //  var amigos = [{ nombre: 'toni', edad: 33 } , { nombre: 'Emi', edad: 25 }];
-  //  buscarAmigo(amigos, 'toni') // retorna { nombre: 'toni', edad: 33 };
-  
+    // Escribe una funcion que reciba un array de amigos (objetos) y un nombre (string).
+    // Tu función debe devolver el amigo que coincida con el nombre recibido por argumento.
+    // Cada amigo tiene las propiedades nombre y edad.
+    // Ej:
+    //  var amigos = [{ nombre: 'toni', edad: 33 } , { nombre: 'Emi', edad: 25 }];
+    //  buscarAmigo(amigos, 'toni') // retorna { nombre: 'toni', edad: 33 };
+    for (var i = 0; i < amigos.length; i++) {
+        if (amigos[i].nombre === nombre) {
+            return amigos[i];
+        }
+    }
+
 }
 
 function sumArray(array, n) {
-  // Escribir un algoritmo que, dada un arreglo de números ordenados y un número n, 
-  // te devuelva true si alguna combinación de dos números cualesquiera suman n, y devuelva false si ninguna combinación
-  // de dos números sumados da como resultado el número n.
-  // Ej:
-  // sumArray([2,5,7,10,11,15,20], 13)  // retorna true     2+11 suman 13
-  // sumArray([2,5,7,10,11,15,20], 14)  // retorna false
-  // pista: podes usar bucles anidados;
-  
+    // Escribir un algoritmo que, dada un arreglo de números ordenados y un número n, 
+    // te devuelva true si alguna combinación de dos números cualesquiera suman n, y devuelva false si ninguna combinación
+    // de dos números sumados da como resultado el número n.
+    // Ej:
+    // sumArray([2,5,7,10,11,15,20], 13)  // retorna true     2+11 suman 13
+    // sumArray([2,5,7,10,11,15,20], 14)  // retorna false
+    // pista: podes usar bucles anidados;
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] + array[j] === n) {
+                return true
+            }
+        }
+    }
+    return false;
 };
 
-function pluck(array, propiedad) { 
-  // Escribi una función llamada pluck,
-  // que recibe un array de objetos (array) y el nombre de una propiedad (propiedad).
-  // La función va a devolver un nuevo arreglo con solo los
-  // valores dentro de la propiedad recibida, por ejemplo:
-  // var productos = [{ name: 'TV LCD', price: 100}, { name: 'Computadora', price: 500 }]
-  // productos.pluck(productos, 'name') // ['TV LCD', 'Computadora']
-  // pista: es una buena oportunidad para usar map.
-  
-  
+function pluck(array, propiedad) {
+    // Escribi una función llamada pluck,
+    // que recibe un array de objetos (array) y el nombre de una propiedad (propiedad).
+    // La función va a devolver un nuevo arreglo con solo los
+    // valores dentro de la propiedad recibida, por ejemplo:
+    // var productos = [{ name: 'TV LCD', price: 100}, { name: 'Computadora', price: 500 }]
+    // productos.pluck(productos, 'name') // ['TV LCD', 'Computadora']
+    // pista: es una buena oportunidad para usar map.
+    nameProductos = array.map(function(e) {
+        return e[propiedad];
+    })
+    return nameProductos;
+
 }
 
 // =======================================================================
 
 
 function crearClasePersona() {
-  // Crear una clase para construir objeto de tipo Persona.
-  // el constructor debe recibir:
-  // nombre (string) , edad (integer) , hobbies (array de strings) , amigos (array de objetos)
-  // Esta funcion debe retonar la clase Persona.
+    // Crear una clase para construir objeto de tipo Persona.
+    // el constructor debe recibir:
+    // nombre (string) , edad (integer) , hobbies (array de strings) , amigos (array de objetos)
+    // Esta funcion debe retonar la clase Persona.
 
-  class Persona {
-    constructor(nombre, edad, hobbies, amigos) {
-      
-    }
+    class Persona {
+        constructor(nombre, edad, hobbies, amigos) {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.hobbies = hobbies;
+            this.amigos = amigos
 
-    addFriend(nombre, edad) {
-      // el metodo addFriend recibe un string nombre y un entero edad y debe agregar un objeto:
-      // { nombre: nombre, edad: edad} al arreglo de amigos de la persona.
-      // no debe retornar nada.
-      
-    }
+        }
 
-    addHobby(hobby) {
-      // este método debe agregar un hobby (hobby) al arreglo de hobbies de la persona.
-      // no debe retornar nada.
-      
-    }
-    getFriends() {
-      // Escribe una función que retorne un arreglo con sólo los nombres del arreglo de amigos
-      // de la persona.
-      // Ej:
-      // persona.getFriends() // retorna ['toni', 'Leo', 'Manu']
-      
-    }
+        addFriend(nombre, edad) {
+            // el metodo addFriend recibe un string nombre y un entero edad y debe agregar un objeto:
+            // { nombre: nombre, edad: edad} al arreglo de amigos de la persona.
+            // no debe retornar nada.
 
-    getHobbies() {
-      // Escribe una función que retorne un arreglo con los hobbies de la persona
-      // Ej:
-      // persona.getHobbies() // retorna ['correr', 'dormir', 'nadar']
-      
-    }
+            this.amigos.push({ nombre, edad });
 
-    getPromedioEdad() {
-      // Escribe una función que retorne el promedio de edad de los amigos de una persona
-      // ej, si la persona tuviera estos amigos:
-      // {
-      //   amigos: [{
-      //     nombre: 'toni',
-      //     edad: 33,
-      //   }, {
-      //     nombre: 'Emi',
-      //     edad: 25
-      //   }]
-      // }
-      // persona.getPromedioEdad() // retorna 29
-    }
-  };
 
-  return Persona;
+        }
+
+        addHobby(hobby) {
+            // este método debe agregar un hobby (hobby) al arreglo de hobbies de la persona.
+            // no debe retornar nada.
+
+            this.hobbies.push(hobby);
+        }
+        getFriends() {
+            // Escribe una función que retorne un arreglo con sólo los nombres del arreglo de amigos
+            // de la persona.
+            // Ej:
+            // persona.getFriends() // retorna ['toni', 'Leo', 'Manu']
+
+            var nombreAmigos = this.amigos.map(function(e) {
+                return e.nombre;
+            })
+            return nombreAmigos;
+        }
+
+        getHobbies() {
+            // Escribe una función que retorne un arreglo con los hobbies de la persona
+            // Ej:
+            // persona.getHobbies() // retorna ['correr', 'dormir', 'nadar']
+            this.hobbies.map(function(e) {
+                return e.hobbies;
+            });
+            return this.hobbies;
+
+        }
+
+        getPromedioEdad() {
+            // Escribe una función que retorne el promedio de edad de los amigos de una persona
+            // ej, si la persona tuviera estos amigos:
+            // {
+            //   amigos: [{
+            //     nombre: 'toni',
+            //     edad: 33,
+            //   }, {
+            //     nombre: 'Emi',
+            //     edad: 25
+            //   }]
+            // }
+            // persona.getPromedioEdad() // retorna 29
+            return this.amigos.reduce(function(acc, e) {
+                return acc + e.edad
+            }, 0) / this.amigos.length
+        }
+    };
+
+    return Persona;
 }
 
 
@@ -116,36 +174,36 @@ function crearClasePersona() {
 // pero si llegas hasta aca y lo haces quiere decir que venis super bien!
 
 function filtrar(funcion) {
-  // Escribi una función filtrar en el prototipo de Arrays,
-  // que recibe una funcion (callback) que devuelve true o false.
-  // filtrar los elementos de ese arreglo en base al resultado de esa funcion
-  // comparadora, devolver un nuevo arreglo con los elementos filtrados.
-  // NO USAR LA FUNCION FILTER DE LOS ARREGLOS.
-  // ej:
-  // var productos = [{
-  //   price: 100,
-  //   name: 'tv'
-  // }, {
-  //   price: 50,
-  //   name: 'phone'
-  // }, {
-  //   price: 30,
-  //   name: 'lamp'
-  // }]
-  // productos.filtrar(function(p) {
-  //   return p.price >= 50;
-  // }) => [{price: 100, name:'tv'}]
+    // Escribi una función filtrar en el prototipo de Arrays,
+    // que recibe una funcion (callback) que devuelve true o false.
+    // filtrar los elementos de ese arreglo en base al resultado de esa funcion
+    // comparadora, devolver un nuevo arreglo con los elementos filtrados.
+    // NO USAR LA FUNCION FILTER DE LOS ARREGLOS.
+    // ej:
+    // var productos = [{
+    //   price: 100,
+    //   name: 'tv'
+    // }, {
+    //   price: 50,
+    //   name: 'phone'
+    // }, {
+    //   price: 30,
+    //   name: 'lamp'
+    // }]
+    // productos.filtrar(function(p) {
+    //   return p.price >= 50;
+    // }) => [{price: 100, name:'tv'}]
 
 };
 
 // No modifiques nada debajo de esta linea
 // 
 module.exports = {
-  sumArray,
-  menorMayor,
-  stringMasLarga,
-  buscarAmigo,
-  pluck,
-  crearClasePersona,
-  filtrar
+    sumArray,
+    menorMayor,
+    stringMasLarga,
+    buscarAmigo,
+    pluck,
+    crearClasePersona,
+    filtrar
 }
--- teoria.md
+++ teoria.md
@@ -1,24 +1,24 @@
 ## Explica los siguentes conceptos con tus propias palabras. (no más de tres líneas por respuesta).
 
-* HTML:
+* HTML: lenguaje de marcado de hipertexto, sirve para crear la estructura de una pagina web.
 
-* CSS:
+* CSS:hoja de estilo en cascada, sirve para dar estilos a una pagina web, como por ejemplo darle un color de fondo, definir el tipo de letra, tamaño , fuente, etc.
 
-* Git:
+* Git: es una herramienta de control de versiones, sirve en el caso de proyectos que requieran la intervencion de mas de un programador(trabajo colaborativo)
 
-* Arreglos:
+* Arreglos: es un tipo de dato, que puede contener un conjunto de otros datos como ser strings, numbers, arreglos, objetos. estan contenidos entre corchetes.
 
-* Objetos:
+* Objetos: es un tipo de dato, que brinda informacion acerca de un elemento en particular, objeto, etc, cuya caracteristica es que la informacion se expresa mediante pares key-value, o llave-valor.
 
-* Variables:
+* Variables: es una unidad de almacenamiento de informacion, en javascript se la representa mediante una palabra reservada y a continuacion el nombre que llevara la misma, que tiene que ser representativo de lo que se esta haciendo.
 
-* Métodos:
+* Métodos: son funciones que estan contenidas dentro de un objeto, y que ejecutan una propiedad del mismo.
 
-* DOM:
+* DOM: document object model es una interfaz de aplicaciones, que define la estructura de un documento del tipo HTML.
 
-* Atributo de un tag HTML:
+* Atributo de un tag HTML: un atributo agrega informacion adicional acerca de un elemento en particular ejemplo de atributo:  "id", "style", etc.
 
-* Selector CSS:
+* Selector CSS: a traves de los selectores css podemos seleccionar un elemento del HTML y aplicarle los estilos apropiados a lo que queremos lograr.
 
-* Un función constructora:
+* Un función constructora: estas son asimilables a moldes,  sirven para crear objetos con las propiedades definidas en la misma funcion constructora.
 
